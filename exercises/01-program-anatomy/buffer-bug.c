c7eded48db7552ad9ee5c898b0915ece06ecd0c0
Samuel Mancera <samuelmancera.097@gmail.com>
2026-09-08 22:17:35 -0500

    feat: add buffer bug exercise

---
+++ exercises/01-program-anatomy/buffer-bug.c
@@ -0,0 +1,20 @@
+/* Declarating external functions to use (libraries) */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Entry point to indicate where the program starts */
+int main () {
+    /* Initialize variables */
+    int num;
+    char answer;
+    /* Ask for a random number */
+    printf("Please enter a random number: ");
+    /* Save the input value in the num variable */
+    scanf("%d", &num);
+    /* Ask for another value to experience buffer '\n' bug and skip input */
+    printf("Do you want to confirm your input? [Y/N]: ");
+    /* Normal instruction to read normal value */
+    scanf("%c", &answer);
+    /* Use 'stdlib.h' to use descriptiving messages */
+    return EXIT_SUCCESS;
+}
