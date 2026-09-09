43ac1f1bb13e467f68be24ac03e7adffc49af2b4
Samuel Mancera <samuelmancera.097@gmail.com>
2026-09-08 21:35:44 -0500

    feat: add undefined behavior exercise

---
+++ exercises/01-program-anatomy/undefined-behavior.c
@@ -0,0 +1,14 @@
+/* Declarating external functions to use (libraries) */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Entry point to indicate where the program starts */
+int main () {
+    /* Initialize variables */
+    int num = 15;
+    char topic [20] = "programming";
+    /* Standard output with a mistake between the argument and its format especifier to provoke undefined behavior */
+    printf("Today %s is the topic we are going to work with", num);
+    /* Use 'stdlib.h' to use descriptiving messages */
+    return EXIT_SUCCESS;
+}
