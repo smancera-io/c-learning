0cb9a8a519b8591f79222b367811ee62d279026b
Samuel Mancera <samuelmancera.097@gmail.com>
2026-09-09 21:33:38 -0500

    docs: describe 'include.c' and 'library.h' files

--- exercises/01-program-anatomy/include.c
+++ exercises/01-program-anatomy/include.c
@@ -1,11 +1,18 @@
+/* Declarating external functions to use (libraries) */
+#include "library.h" /* We use quotation marks to indicate an own library in our folder */
 #include "library.h"
-#include "library.h"
-#include <stdio.h>
+#include <stdio.h>   /* We use angle brackets to indicate it is a system library */
 
+/* Entry point to indicate where the program starts */
 int main () {
+    /* Initialize variables*/
     int a, b;
+    /* Ask user for two numbers */
     printf("Please enter two numbers separated by a space: ");
+    /* Read and save the values */
     scanf("%d %d", &a, &b);
+    /* Call our own function from 'library.h' and save its result*/
     int result = sum(a, b);
+    /* Print with standard output the final result */
     printf("The result is %d", result);
 }
--- exercises/01-program-anatomy/library.h
+++ exercises/01-program-anatomy/library.h
@@ -1,4 +1,6 @@
+/* Sentence to avoid generating diamong pattern or circular dependency, loading twice the same piece of code */
 #pragma once
+/* Own fuction store in a header file */
 int sum (int a, int b) {
     return (a+b);
 }
